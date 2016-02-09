845b7e734201c0fa9d3389765f5c3b03711f5f1c
Hampus <haca89@gmail.com>
2016-02-09 10:37:07 +0100

    Committing data pair tests.

---
+++ Inlupp1_laboration1/test/temp/MatchedDataPairTest.java
@@ -0,0 +1,39 @@
+package temp;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MatchedDataPairTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMatchedDataPair() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetXValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetYValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
